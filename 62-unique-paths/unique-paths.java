9bc89aba798539c7be99171a571e72e232987f96
Gautam Kumar <119605352+gtm001@users.noreply.github.com>
2025-07-16 03:03:39 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (88.56%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,5 +1,11 @@
 class Solution {
     public int uniquePaths(int m, int n) {
+        if(m==0||n==0){
+            return 0;
+        }
+        if(m==1 && n==1){
+            return 1;
+        }
         int[][]dp = new int[m+1][n+1];
         // pre-populate
         for(int i=0;i<=n;i++){
