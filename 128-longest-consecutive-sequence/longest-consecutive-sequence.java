7953285e016f1346a7235ab1a8ec5f76e2c289a2
Gautam Kumar <119605352+gtm001@users.noreply.github.com>
2025-07-27 22:13:09 +0530

    Time: 31 ms (57.62%) | Memory: 67.6 MB (47.57%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -1,24 +1,47 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
-        int n = nums.length;
-        int maxL = 0;
-        int l = 1;
-        if(nums.length == 0){
-            return 0;
+
+    // ----------- Using Array Sort, Time Complexity - O(nlogn) -----------------------
+    //     int n = nums.length;
+    //     int maxL = 0;
+    //     int l = 1;
+    //     if(nums.length == 0){
+    //         return 0;
+    //     }
+    //     Arrays.sort(nums);
+    //     for(int i=0;i<n;i++){
+    //         if(i>0 && nums[i]==nums[i-1]){
+    //             continue;
+    //         }
+    //         else if(i>0 && nums[i]==nums[i-1]+1){
+    //             l++;
+    //         }
+    //         else{
+    //             l = 1;
+    //         }
+    //         maxL = Math.max(maxL,l);
+    //     }
+    //     return maxL;
+    // }
+
+    // ----------- using HashSet , Time-Complexity O(n) -----------------------------------
+     HashSet<Integer> set = new HashSet<>();
+     for(int num:nums){
+        set.add(num);
+     }
+     int len = 0;
+     int maxLen = 0;
+     for(int val:set){
+        if(!set.contains(val-1)){
+            int currVal = val;
+            len = 1;
+            while(set.contains(currVal+1)){
+                currVal++;
+                len++;
+                }
+                maxLen = Math.max(maxLen,len);
+            }
         }
-        Arrays.sort(nums);
-        for(int i=0;i<n;i++){
-            if(i>0 && nums[i]==nums[i-1]){
-                continue;
-            }
-            else if(i>0 && nums[i]==nums[i-1]+1){
-                l++;
-            }
-            else{
-                l = 1;
-            }
-            maxL = Math.max(maxL,l);
-        }
-        return maxL;
+        return maxLen;
     }
 }
