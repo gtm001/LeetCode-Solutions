56f2717405258b2c8795eac13dd514ce5a9501e4
Gautam Kumar <119605352+gtm001@users.noreply.github.com>
2025-08-05 13:04:33 +0530

    Time: 1 ms (100.00%) | Memory: 46.4 MB (12.21%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,23 +1,56 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        // int n1 = nums1.length;
+        // int n2 = nums2.length;
+        // int l = n1+n2;
+        // int newA[] = new int[l];
+        // for(int i=0;i<n1;i++){
+        //     newA[i] = nums1[i];
+        // }
+        // for(int i=0;i<n2;i++){
+        //     newA[n1+i] = nums2[i];
+        // }
+        // Arrays.sort(newA);
+        // double med = 0;
+        // if((l%2)!=0){
+        //     med = newA[l/2];
+        // }
+        // else{
+        //     med = (newA[l / 2] + newA[(l / 2) - 1]) / 2.0;
+        // }
+        // return med;
+
+        // --------- Also without using Array Sort(Using Merge Sort)-------------
         int n1 = nums1.length;
         int n2 = nums2.length;
-        int l = n1+n2;
-        int newA[] = new int[l];
-        for(int i=0;i<n1;i++){
-            newA[i] = nums1[i];
+        int n = n1+n2;
+        int newA[] = new int[n];
+        int k = 0; // to track the index of new Array!
+
+        int i = 0;
+        int j = 0;
+        while(i<n1 && j<n2){
+            if(nums1[i]>nums2[j]){
+                newA[k++] = nums2[j++];
+            }
+            else{
+                newA[k++] = nums1[i++];
+            }
         }
-        for(int i=0;i<n2;i++){
-            newA[n1+i] = nums2[i];
+        while(i<n1){
+            newA[k++] = nums1[i++];
         }
-        Arrays.sort(newA);
+        while(j<n2){
+            newA[k++] = nums2[j++];
+        }
         double med = 0;
-        if((l%2)!=0){
-            med = newA[l/2];
+        if((n%2)!=0){
+        med = newA[n/2];
         }
         else{
-            med = (newA[l / 2] + newA[(l / 2) - 1]) / 2.0;
-        }
+            med = (newA[n / 2] + newA[(n / 2) - 1]) / 2.0;
+             }
         return med;
+
     }
 }
