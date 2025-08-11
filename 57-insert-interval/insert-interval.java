fba3b79b6f0484d0a41cd1ff9c131a6fd97df3fc
Gautam Kumar <119605352+gtm001@users.noreply.github.com>
2025-08-11 10:24:39 +0530

    Time: 1 ms (98.91%) | Memory: 45 MB (59.55%) - LeetSync

--- 57-insert-interval/insert-interval.java
+++ 57-insert-interval/insert-interval.java
@@ -1,71 +1,34 @@
-// class Solution {
-//     public int[][] insert(int[][] intervals, int[] newInterval) {
-//         List<List<Integer>> list = new ArrayList<>();
-//         for(int i=0;i<intervals.length;i++){
-//                 List<Integer> arr = new ArrayList<>();
-//                 if((newInterval[0]<intervals[i][0]) && (newInterval[1]<intervals[i][1])){
-//                     arr.add(intervals[i][0]);
-//                     arr.add(intervals[i][1]);
-//                     list.add(arr);
-//                 }
-//                 else if(newInterval[0]<intervals[i][0] && i>0){
-//                     arr.add(intervals[i-1][0]);
-//                     list.add(Arrays.asList(arr));
-//                 }
-//                 else if(newInterval[1]<intervals[i][1]){
-//                     arr.add(intervals[i][1]);
-//                     list.add(Arrays.asList(arr));
-//                 }
-//                 else{
-//                     arr.add(intervals[i][0]);
-//                     arr.add(intervals[i][1]);
-//                     list.add(Arrays.asList(arr));
-//                 }
-//             }
-
-//             // copyingg things in 2d array;
-//             int tdarr[][] = new int[arr.size()][2];
-//             for(int i=0;i<arr.size();i++){
-//                 for(int j=0;i<2;j++){
-//                     tdarr[i][j] = list.get(i).get(j);
-//                 }
-//             }
-//         }
-//         return tdarr;
-//     }
-// }
-
 class Solution {
-        public int[][] insert(int[][] intervals, int[] newInterval) {
-                List<int[]> list = new ArrayList<>();
-                        int i = 0;
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        List<int[]> list = new ArrayList<>();
+        int i = 0;
+        while(i<intervals.length && intervals[i][1]<newInterval[0]){
+            list.add(intervals[i]);
+            i++;
+        }
+        while(i<intervals.length && intervals[i][0]<=newInterval[1]){
+            newInterval[0] = Math.min(newInterval[0],intervals[i][0]);
+            newInterval[1] = Math.max(newInterval[1],intervals[i][1]);
+            i++;
+        }
+        list.add(newInterval);
+        while(i<intervals.length){
+            list.add(intervals[i]);
+            i++;
+        }
 
-                                // Add all intervals before newInterval
-                                        while (i < intervals.length && intervals[i][1] < newInterval[0]) {
-                                                    list.add(intervals[i]);
-                                                                i++;
-                                                                        }
 
-                                                                                // Merge overlapping intervals
-                                                                                        while (i < intervals.length && intervals[i][0] <= newInterval[1]) {
-                                                                                                    newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
-                                                                                                                newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
-                                                                                                                            i++;
-                                                                                                                                    }
-                                                                                                                                            list.add(newInterval);
+    int[][] res = new int[list.size()][2];
+    // for(int k=0;k<res.length;k++){
+    //     for(int j=0;j<2;j++){
+    //         res[k][j] = list.get(k)[j];
+    //     }
+    // }
+    // don't need double for loop, coz since we do have taken List<int[]>
 
-                                                                                                                                                    // Add remaining intervals
-                                                                                                                                                            while (i < intervals.length) {
-                                                                                                                                                                        list.add(intervals[i]);
-                                                                                                                                                                                    i++;
-                                                                                                                                                                                            }
-
-                                                                                                                                                                                                    // Convert list to 2D array
-                                                                                                                                                                                                            int[][] result = new int[list.size()][2];
-                                                                                                                                                                                                                    for (int j = 0; j < list.size(); j++) {
-                                                                                                                                                                                                                                result[j] = list.get(j);
-                                                                                                                                                                                                                                        }
-                                                                                                                                                                                                                                                return result;
-                                                                                                                                                                                                                                                    }
-                                                                                                                                                                                                                                                    }
-
+    for(int j=0;j<res.length;j++){
+        res[j] = list.get(j);
+    }
+    return res;
+    }
+}
