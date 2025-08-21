0c6631934adf1236f47b13f3ffa066056142b50a
Gautam Kumar <119605352+gtm001@users.noreply.github.com>
2025-08-21 17:22:45 +0530

    Time: 4 ms (77.35%) | Memory: 43.1 MB (76.17%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] arr1 = new int[26];
+        int[] arr2 = new int[26];
+        if(s.length()!=t.length()){
+            return false;
+        }
+        int n = s.length();
+        int c1 = 0;
+        int c2 = 0;
+        for(int i=0;i<n;i++){
+            char ch = s.charAt(i);
+            arr1[ch-'a']++;
+            c1 += arr1[ch-'a'];
+        }
+
+        for(int i=0;i<n;i++){
+            char ch = t.charAt(i);
+            arr2[ch-'a']++;
+            c2 += arr2[ch-'a'];
+        }
+
+         for(int i=0;i<26;i++){
+                if(arr1[i] != arr2[i]){
+                        return false; // mismatch in frequency
+                            }
+
+                             
+         }
+         return true;
+    }
+}
