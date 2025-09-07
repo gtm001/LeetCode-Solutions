2ba87835987412e9f9db3b4d4b8201465a6ad00c
Gautam Kumar <119605352+gtm001@users.noreply.github.com>
2025-09-07 08:02:30 +0530

    Time: 2561 ms (5.09%) | Memory: 45.6 MB (21.42%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,60 +1,60 @@
-// class Solution {
-//     public String longestPalindrome(String s) {
-//         if(s.length()<=1){
-//             return s;
-//         }
-//         String res = "";
-//         for(int i=0;i<s.length();i++){
-//             for(int j=i;j<s.length();j++){
-//                 String sub = s.substring(i,j+1);
-//                 if(isPelindrome(sub)){
-//                     if(sub.length()>res.length()){
-//                         res = sub;
-//                     }
-//                 }
-//             }
-//         }
-//         return res;
-//     }
-//     public static boolean isPelindrome(String s){
-//         int l = 0;
-//         int r = s.length()-1;
-//         while(l<r){
-//             if(s.charAt(l)!=s.charAt(r)){
-//                 return false;
-//             }
-//             l++;
-//             r--;
-//         }
-//         return true;
-//     }
-// }
-
-// ------- Above one is not optimize in term of Space Complexity! - --------- so let's optimize it,-------------
 class Solution {
     public String longestPalindrome(String s) {
         if(s.length()<=1){
             return s;
         }
-        int maxLen = 1;
-        int start = 0;
+        String res = "";
         for(int i=0;i<s.length();i++){
-            int l1 = expCenLen(s,i,i); //odd len
-            int l2 = expCenLen(s,i,i+1); //even len
-            int len = Math.max(l1,l2);
-            if(len>maxLen){
-                maxLen = len;
-                start = i-(len-1)/2;
+            for(int j=i;j<s.length();j++){
+                String sub = s.substring(i,j+1);
+                if(isPelindrome(sub)){
+                    if(sub.length()>res.length()){
+                        res = sub;
+                    }
+                }
             }
         }
-        return s.substring(start,start+maxLen);
+        return res;
     }
-    public static int expCenLen(String s,int left,int right){
- 
-        while(left>=0 && right<s.length() && s.charAt(left)==s.charAt(right)){
-            left--;
-            right++;
+    public static boolean isPelindrome(String s){
+        int l = 0;
+        int r = s.length()-1;
+        while(l<r){
+            if(s.charAt(l)!=s.charAt(r)){
+                return false;
+            }
+            l++;
+            r--;
         }
-    return right-left-1;       
+        return true;
     }
 }
+
+// // ------- Above one is not optimize in term of Space Complexity! - --------- so let's optimize it,-------------
+// class Solution {
+//     public String longestPalindrome(String s) {
+//         if(s.length()<=1){
+//             return s;
+//         }
+//         int maxLen = 1;
+//         int start = 0;
+//         for(int i=0;i<s.length();i++){
+//             int l1 = expCenLen(s,i,i); //odd len
+//             int l2 = expCenLen(s,i,i+1); //even len
+//             int len = Math.max(l1,l2);
+//             if(len>maxLen){
+//                 maxLen = len;
+//                 start = i-(len-1)/2;
+//             }
+//         }
+//         return s.substring(start,start+maxLen);
+//     }
+//     public static int expCenLen(String s,int left,int right){
+ 
+//         while(left>=0 && right<s.length() && s.charAt(left)==s.charAt(right)){
+//             left--;
+//             right++;
+//         }
+//     return right-left-1;       
+//     }
+// }
