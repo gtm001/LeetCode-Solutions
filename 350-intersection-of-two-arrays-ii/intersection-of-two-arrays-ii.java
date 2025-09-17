aae8a143f4665598c6ca631b70ba315720785a83
Gautam Kumar <119605352+gtm001@users.noreply.github.com>
2025-09-17 09:09:42 +0530

    Time: 3 ms (91.07%) | Memory: 43.2 MB (68.37%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
@@ -1,16 +1,39 @@
+// class Solution {
+//     public int[] intersect(int[] nums1, int[] nums2) {
+//         int frq[] = new int[1001];
+//         for(int i=0;i<nums1.length;i++){
+//             frq[nums1[i]-0]++;
+//         }
+//         ArrayList<Integer> arr = new ArrayList<>();
+//         for(int val:nums2){
+//             if(frq[val]>0){
+//                 arr.add(val);
+//                 frq[val]--;
+//             }
+//         }
+//         int res[] = new int[arr.size()];
+//         for(int i=0;i<res.length;i++){
+//             res[i] = arr.get(i);
+//         }
+//         return res;
+//     }
+// }
+
+// -------------- Using HashMap ------------------
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        int frq[] = new int[1001];
+        HashMap<Integer,Integer> map = new HashMap<>();
         for(int i=0;i<nums1.length;i++){
-            frq[nums1[i]-0]++;
+            map.put(nums1[i],map.getOrDefault(nums1[i],0)+1);
         }
         ArrayList<Integer> arr = new ArrayList<>();
         for(int val:nums2){
-            if(frq[val]>0){
+            if(map.getOrDefault(val,0)>0){
                 arr.add(val);
-                frq[val]--;
+                map.put(val,map.get(val)-1);
             }
         }
+
         int res[] = new int[arr.size()];
         for(int i=0;i<res.length;i++){
             res[i] = arr.get(i);
