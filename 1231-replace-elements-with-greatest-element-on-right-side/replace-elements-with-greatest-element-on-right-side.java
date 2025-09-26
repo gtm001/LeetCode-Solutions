5cd7d92b5eda2de7b6186b75a13eaf1b45f72693
Gautam Kumar <119605352+gtm001@users.noreply.github.com>
2025-09-26 12:34:05 +0530

    Time: 988 ms (5.06%) | Memory: 46.8 MB (50.78%) - LeetSync

--- 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.java
+++ 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.java
@@ -1,12 +1,12 @@
 class Solution {
     public int[] replaceElements(int[] arr) {
         int res[] = new int[arr.length];
-        for(int i=0;i<res.length;i++){
+        for(int i=0;i<res.length-1;i++){
             int maxv = Integer.MIN_VALUE;
             for(int j=i+1;j<res.length;j++){
                 maxv = Math.max(maxv,arr[j]);
-                res[i]=maxv;
             }
+                res[i]=maxv;
         }
         res[res.length-1] = -1;
         return res;
