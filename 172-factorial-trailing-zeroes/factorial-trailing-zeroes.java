38c654d91b04173547488dcc5728f30eeb9b655e
Gautam Kumar <119605352+gtm001@users.noreply.github.com>
2025-10-11 21:19:30 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (49.23%) - LeetSync

---
+++ 172-factorial-trailing-zeroes/factorial-trailing-zeroes.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int trailingZeroes(int n) {
+        int sum = 0;
+        int f = 5;
+        while(n/f>0){
+            sum+=n/f;
+            f=f*5;
+        }
+        return sum;
+    }
+}
