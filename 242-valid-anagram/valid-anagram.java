7f7fd43ac22d4d3f63fc1d2efacf2197e0533a3a
Gautam Kumar <119605352+gtm001@users.noreply.github.com>
2025-12-11 05:01:11 +0530

    Time: 4 ms (78.84%) | Memory: 44.4 MB (61.98%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,49 +1,17 @@
 class Solution {
-    // public boolean isAnagram(String s, String t) {
-    //     int[] arr1 = new int[26];
-    //     int[] arr2 = new int[26];
-    //     if(s.length()!=t.length()){
-    //         return false;
-    //     }
-    //     int n = s.length();
-    //     int c1 = 0;
-    //     int c2 = 0;
-    //     for(int i=0;i<n;i++){
-    //         char ch = s.charAt(i);
-    //         arr1[ch-'a']++;
-    //         c1 += arr1[ch-'a'];
-    //     }
-
-    //     for(int i=0;i<n;i++){
-    //         char ch = t.charAt(i);
-    //         arr2[ch-'a']++;
-    //         c2 += arr2[ch-'a'];
-    //     }
-
-    //      for(int i=0;i<26;i++){
-    //             if(arr1[i] != arr2[i]){
-    //                     return false; // mismatch in frequency
-    //             }
-
-                             
-    //      }
-    //      return true;
-    // }
-
-    // --------- Sorting the code ------------------
     public boolean isAnagram(String s, String t) {
-        if(s.length()!=t.length()){
-            return false;
-        }
-        int n = s.length();
         int arr[] = new int[26];
-        for(int i=0;i<n;i++){
-            arr[s.charAt(i)-'a']++;
-            arr[t.charAt(i)-'a']--;
+        for(int i=0;i<s.length();i++){
+            char c = s.charAt(i);
+            arr[c-'a']++;
         }
-        for(int val:arr){
-            if(val!=0){
-                return false;
+        for(int i=0;i<t.length();i++){
+            char c2 = t.charAt(i);
+            arr[c2-'a']--;
+        }
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]!=0){
+                return false; 
             }
         }
         return true;
